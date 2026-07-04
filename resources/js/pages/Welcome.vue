bf2554c2bac0ec229ff4c120a55b1a8315da637d
Kim <kimrune@hotmail.no>
2026-07-04 15:07:23 +0200

    remove unused imports

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,13 +1,6 @@
 <script setup lang="ts">
-import { Head, Link, usePage } from '@inertiajs/vue3';
-import { computed } from 'vue';
-import { dashboard, login } from '@/routes';
-import { register } from '@/routes';
+import { Head } from '@inertiajs/vue3';
 
-const page = usePage();
-const dashboardUrl = computed(() =>
-    page.props.currentTeam ? dashboard(page.props.currentTeam.slug).url : '/',
-);
 </script>
 
 <template>
@@ -19,7 +12,8 @@ const dashboardUrl = computed(() =>
         class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-white lg:justify-center lg:p-8 dark:bg-[#0a0a0a]"
     >
         <main>
-            Welcome
+            <div>Welcome to demo app</div>
+           
         </main>
         <div class="hidden h-14.5 lg:block"></div>
     </div>
