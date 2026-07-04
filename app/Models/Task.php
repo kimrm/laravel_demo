1900f0c5eef1876ba0e22409a96084828b14deda
Kim <kimrune@hotmail.no>
2026-07-04 15:34:29 +0200

    add fillable and cast

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 
+#[Fillable(['title', 'description', 'completed'])]
 class Task extends Model
 {
-    //
+    protected function casts(): array
+    {
+        return [
+            'completed' => 'boolean',
+        ];
+    }
 }
