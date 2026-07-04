c52e6a3c5805a6543152909c5a46886c6be93e93
Kim <kimrune@hotmail.no>
2026-07-04 15:34:56 +0200

    add return types and throws exceptions

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,62 +4,65 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TaskController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        $tasks = Task::all();
+
+        return response($tasks);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): Response
     {
-        //
+        throw new \Exception('Not implemented');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): Response
     {
-        //
+        throw new \Exception('Not implemented');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Task $task)
+    public function show(Task $task): Response
     {
-        //
+        throw new \Exception('Not implemented');    
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Task $task)
+    public function edit(Task $task): Response
     {
-        //
+        throw new \Exception('Not implemented');
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Task $task)
+    public function update(Request $request, Task $task): Response
     {
-        //
+        throw new \Exception('Not implemented');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Task $task)
+    public function destroy(Task $task): Response
     {
-        //
+        throw new \Exception('Not implemented');
     }
 }
